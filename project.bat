efdb045a8b297cb9e040ca278bd9e32cc1cedc08
Sergey <surzhiks.85@mail.ru>
2021-04-07 19:07:02 +0300

    second commit

--- project.bat
+++
@@ -1,4 +0,0 @@
-chcp 1251 >nul
-start "" browser-sync start --server --files "*/**, *.*"
-start "" "D:\Веб програмирование\Инструменты разработчика\Sublime Text Build 3126 x64\sublime_text.exe"
-start "" "D:\Веб програмирование\Создание сайта\Alexis\alexis-homepage.psd"
